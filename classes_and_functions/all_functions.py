9a6394bd7b0d644a6028e0185f5b7c68956cb991
Ashtonn77 <ashtonrbay@gmail.com>
2020-06-12 15:29:48 +0200

    added seperate module for all functions and classes

---
+++ classes_and_functions/all_functions.py
@@ -0,0 +1,44 @@
+def take_bet():
+
+    pass
+
+
+def hit(deck, hand):
+
+    pass
+
+
+def hit_or_stand(deck, hand):
+    global playing  # to control an upcoming while loop
+
+    pass
+
+
+def show_some(player, dealer):
+
+    pass
+
+
+def show_all(player, dealer):
+
+    pass
+
+
+def player_busts():
+    pass
+
+
+def player_wins():
+    pass
+
+
+def dealer_busts():
+    pass
+
+
+def dealer_wins():
+    pass
+
+
+def push():
+    pass
---
+++ game.py
@@ -0,0 +1,39 @@
+from classes_and_functions import card
+from classes_and_functions import deck
+from classes_and_functions import hand
+from classes_and_functions import chips
+from classes_and_functions import all_functions
+
+while True:
+    # Print an opening statement
+
+    # Create & shuffle the deck, deal two cards to each player
+
+    # Set up the Player's chips
+
+    # Prompt the Player for their bet
+
+    # Show cards (but keep one dealer card hidden)
+
+    while playing:  # recall this variable from our hit_or_stand function
+
+        # Prompt for Player to Hit or Stand
+
+        # Show cards (but keep one dealer card hidden)
+
+        # If player's hand exceeds 21, run player_busts() and break out of loop
+
+            break
+
+    # If Player hasn't busted, play Dealer's hand until Dealer reaches 17
+
+        # Show all cards
+
+        # Run different winning scenarios
+
+    # Inform Player of their chips total
+
+    # Ask to play again
+
+        break
+
